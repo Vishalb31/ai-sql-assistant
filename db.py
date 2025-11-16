355cc3662cb91c8353252cf1d20bb4276065d595
Vishalb31 <vishalbhagat801@gmail.com>
2025-11-16 15:53:36 +0530

    Fix DB auto select

--- db.py
+++ db.py
@@ -1,21 +1,29 @@
 import mysql.connector
 import os
+import streamlit as st
 
 db = mysql.connector.connect(
-    host=os.getenv("MYSQLHOST"),
-    user=os.getenv("MYSQLUSER"),
-    password=os.getenv("MYSQLPASSWORD"),
-    database=os.getenv("MYSQLDATABASE"),
-    port=int(os.getenv("MYSQLPORT")),
+    host=st.secrets["MYSQLHOST"],
+    user=st.secrets["MYSQLUSER"],
+    password=st.secrets["MYSQLPASSWORD"],
+    port=int(st.secrets["MYSQLPORT"]),
+    database=st.secrets["MYSQLDATABASE"]
 )
+
 cursor = db.cursor()
 
 def execute_sql(sql):
     try:
+        # Always select DB first
+        cursor.execute(f"USE {st.secrets['MYSQLDATABASE']};")
+
         cursor.execute(sql)
+
         if cursor.description:
-            return cursor.fetchall()
+            rows = cursor.fetchall()
+            return rows
         db.commit()
         return "Query executed successfully!"
+
     except mysql.connector.Error as err:
         return f"Execution Error: {err}"
