36bec4902c1291f77517f5c48642291b983f33a3
Vishalb31 <vishalbhagat801@gmail.com>
2025-11-17 16:10:36 +0530

    Fix env variable names for Railway

--- db.py
+++ db.py
@@ -3,32 +3,22 @@ import os
 
 def connect_db():
     return mysql.connector.connect(
-        host=os.environ["MYSQLHOST"],
-        user=os.environ["MYSQLUSER"],
-        password=os.environ["MYSQLPASSWORD"],
-        database=os.environ["MYSQLDATABASE"],
-        port=int(os.environ["MYSQLPORT"]),
-        connection_timeout=10
+        host=os.environ["MYSQL_HOST"],
+        user=os.environ["MYSQL_USER"],
+        password=os.environ["MYSQL_PASSWORD"],
+        database=os.environ["MYSQL_DATABASE"],
+        port=int(os.environ["MYSQL_PORT"])
     )
 
 db = connect_db()
 cursor = db.cursor()
 
 def execute_sql(sql):
-    global db, cursor
     try:
         cursor.execute(sql)
         if cursor.description:
             return cursor.fetchall()
         db.commit()
         return "Query executed successfully!"
-    except mysql.connector.Error:
-        # reconnect automatically
-        db.close()
-        db = connect_db()
-        cursor = db.cursor()
-        cursor.execute(sql)
-        if cursor.description:
-            return cursor.fetchall()
-        db.commit()
-        return "Reconnected & executed successfully!"
+    except mysql.connector.Error as err:
+        return f"Execution Error: {err}"
